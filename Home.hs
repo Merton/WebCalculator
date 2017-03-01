994d4ee8829d1a861be7b91ece239c63749989e1
Merton <mert@localhost.localdomain>
2017-03-01 13:01:02 +0000

    Attempt at separating internal hamlet into an external file

--- Home.hs
+++ Home.hs
@@ -7,20 +7,5 @@ import Yesod.Core
 
 getHomeR :: Handler Html
 getHomeR = defaultLayout $ do
-    setTitle "Minimal Multifile"
-    [whamlet|
-        <h> Basic Operations using Haskell Backend, delivered in JSON and HTML </h>
-        <p>
-            <a href=@{AddR 5 7}>HTML addition
-        <p>
-            <a href=@{AddR 5 7}?_accept=application/json>JSON addition
-        <p>
-            <a href=@{SubtractR 5 7}>HTML subtraction
-        <p>
-            <a href=@{SubtractR 5 7}?_accept=application/json>JSON subtraction
-        <p>
-            <a href=@{MultiplyR 5 7}>HTML multiplication
-        <p>
-            <a href=@{MultiplyR 5 7}?_accept=application/json>JSON multiply
-
-    |]
+    setTitle "Calculator"
+    $(whamletFile "templates/homeTemplate")
