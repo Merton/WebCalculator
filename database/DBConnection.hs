85ed44655d2bc1ed2f931fd7e935cdf1de157bb4
Merton <merton@localhost.localdomain>
2017-03-26 14:12:13 +0100

    organised

--- database/DBConnection.hs
+++
@@ -1,9 +0,0 @@
-module DBConnection where
-
-import Database.SQLite
-
-
-
-insertRecord :: Calculation
-insertRecord (x o y z) = insertRow db calculations Row [()]
-  where db = openConnection calculationsDB
