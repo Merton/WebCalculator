64779fe4b68c87e08118a6d1d4abee8187538b15
Merton <mertlansley@gmail.com>
2017-05-04 14:17:22 +0100

    added related files for form entry

--- Handler/Calculations.hs
+++ Handler/Calculations.hs
@@ -6,22 +6,13 @@ import Foundation
 import Yesod
 import Database.DBConnection
 import CalcStructure
-import Yesod.Table (Table)
-import qualified Yesod.Table as Table
 
-{-
-calculationsTable :: Table site Calculation
-calculationsTable = mempty
-  ++ Table.int "First Operator"   (firstOp)
-  ++ Table.string "Operator"         operator
-  ++ Table.int "Second Operator"  (secondOp)
-  ++ Table.int "Result"           (result)
--}
-getCalculationsR :: Handler Html
+getCalculationsR :: Handler TypedContent
 getCalculationsR = do
   liftIO (initialiseDB)
   calculations <- liftIO (getRecords)
-  defaultLayout $ do
+  selectRep $ do
+    provideRep $ defaultLayout $ do
       setTitle "Calculations"
       addStylesheetRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
       [whamlet|
@@ -44,14 +35,4 @@ getCalculationsR = do
                 <td>#{secondOp c}
                 <td>#{result c}
       |]
-      
-{-
-postCalculationsR :: Calculation -> Handler TypedContent
-postCalculationsR (Calculation x o y z) = do
-  liftIO $ insertRecord (Calculation x o y z)
-  selectRep $ do
-    provideRep $ defaultLayout $ do
-      setTitle "New Calculation Added"
-      [whamlet|<p> The new calculation is: </p> #{x} #{o} #{y} = #{z}|]
-    provideJson $ (Calculation x o y z)
--}
+    provideJson $ calculations
