9eaa2f916f46c79a09110fd27737eeaeab1746ca
Merton <mertlansley@gmail.com>
2017-05-04 14:14:44 +0100

    Added form entry on homepage

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -6,50 +6,15 @@ module Handler.Home where
 import Foundation
 import Yesod.Core
 import CalcStructure
-import Control.Applicative ((<$>), (<*>))
-import Data.Text           (Text)
-import Data.Time           (Day)
-import Yesod
-import Yesod.Form.Jquery
+
 
 --import DBConnection
-
 getHomeR :: Handler Html
-getHomeR = do
-    -- Generate the form to be displayed
-    (widget, enctype) <- generateFormPost calculationForm
-    defaultLayout
-        [whamlet|
-            <p>
-                The widget generated contains only the contents
-                of the form, not the form tag itself. So...
-            <form method=post action=@{CalculationsR} enctype=#{enctype}>
-                ^{widget}
-                <p>It also doesn't include the submit button.
-                <button>Submit
-        |]
-
-postCalculationsR :: Handler Html
-postCalculationsR = do
-            ((result, widget), enctype) <- runFormPost calculationForm
-            case result of
-                FormSuccess calculation -> defaultLayout [whamlet|<p>#{show calculation}|]
-                _ -> defaultLayout
-                    [whamlet|
-                        <p>Invalid input, let's try again.
-                        <form method=post action=@{CalculationsR} enctype=#{enctype}>
-                            ^{widget}
-                            <button>Submit
-                    |]
-calculationAForm :: AForm Handler Calculation
-calculationAForm = Calculation
-    <$> areq intField "First Operator" Nothing
-    <*> areq (selectField operators) "Operand" Nothing
-    <*> areq intField "Second Operator" Nothing
-    <*> aopt intField "Result" Nothing
-      where
-        operators :: [(Char, Char)]
-        operators = [('+', '+'), ('-','-'),('-','-'),('/','/')]
-
-calculationForm :: Html -> MForm Handler (FormResult Calculation, Widget)
-calculationForm = renderTable calculationAForm
+getHomeR = defaultLayout $ do
+  setTitle "Haskell Web Calculator API"
+  addStylesheetRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+  addStylesheetRemote "http://fonts.googleapis.com/css?family=Strait"
+  addScript $ StaticR form_js
+  addStylesheet $ StaticR layoutCSS_css
+  $(whamletFile "templates/layout.html")
+  $(whamletFile "templates/home.html")
